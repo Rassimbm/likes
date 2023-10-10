8823ae7a1ca709a39a027f1265cfd3a0dc9e49cd
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-09 23:45:42 -0500

    updated part2.html, style-sheet and script

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 console.log("page loaded ...");
 
-var count = 3;
-var likeCount = document.querySelector("#count");
+var count = [3,9,12,9];
+var likeCount = [
+    document.querySelector("#count-1"),
+    document.querySelector("#count-2"),
+    document.querySelector("#count-3"),
+    document.querySelector("#count-4")
+]
 
-function add() {
-    count++;
-    likeCount.innerText = count + " like(s)"
+
+function add(id) {
+    count[id]++;
+    likeCount[id].innerText = count[id] + " like(s)"
 }
